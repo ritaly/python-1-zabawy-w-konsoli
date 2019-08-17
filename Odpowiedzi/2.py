fb808c5506a760d0cc8317f42c6e26e3d540b52a
Malgorzata Rita Ły <malgorzata.lyczywek@gmail.com>
2019-08-17 14:41:22 +0200

    Update 2.py
    update formula

--- Odpowiedzi/2.py
+++ Odpowiedzi/2.py
@@ -12,6 +12,6 @@ height = 170
 weight = 63
 age = 25
 S = -161
-PPM = 10 * weight + 6.25 * height + 5 * age + S
+PPM = 10 * weight + 6.25 * height - 5 * age + S
 
 print("Dzienne zapotrzebowanie kaloryczne wynosi:", PPM * 1.6, "kcal")
